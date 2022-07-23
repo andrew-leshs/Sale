5866361656176456e851cf8c3f4ec98f913cfb87
tatjana444 <107064710+tatjana444@users.noreply.github.com>
2022-07-23 19:32:28 +0300

    longer

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -1,35 +1,19 @@
 import java.util.stream.IntStream;
 
 public class SalesManager {
-    protected int[] sales;
+    protected long[] sales;
 
-    public SalesManager(int[] sales) {
+    public SalesManager(long[] sales) {
         this.sales = sales;
     }
 
-    public int max() {
-        int max = -1;
-        for (int sale : sales) {
+    public long max() {
+        long max = -1;
+        for (long sale : sales) {
             if (sale > max) {
                 max = sale;
             }
         }
         return max;
-    }
-
-    public int trimmedMean() {
-        int max = sales[0];
-        int min = sales[0];
-        long sum = 0;
-        for (int i = 0; i < sales.length; i++){
-            sum = sum + sales[i];
-            if (sales[i] > max) {
-                max = sales[i];
-            }
-            if (sales[i] < min) {
-                min = sales[i];
-            }
-        }
-        return (int) (((double) (sum - max - min)) / (sales.length - 2));
     }
 }
